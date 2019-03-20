3cc8816cc9c9a328e8b3288c25495f59fc30c012
dzoli <bje.no9@gmail.com>
2019-03-20 23:23:27 +0100

    init

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Post
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -12,3 +15,23 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    # fields = ['title']
+    fields = '__all__'  # this set all fields from model
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']  # we only want to edit title and body of the post!
+    template_name = 'post_edit.html'
+
+
+# reverse_lazy opposed to reverse (in urls) it won't execute url redirect until view has finished deleting the blog post
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,4 +1,4 @@
-"""blog_app URL Configuration
+"""blog_app URL Configuration -- project-level URLs
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls'))
 ]
